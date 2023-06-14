5b2e2f0e4cf3b8fcfececcf23006acee3fce99cf
Mvanspelden <302197538@student.rocmondriaan.nl>
2023-06-14 11:40:32 +0200

    overgezet

---
+++ Room3/js/main.js
@@ -0,0 +1,5 @@
+function papercode() {
+    console.log('papercode')
+}
+//    <button class="paperCode" type="button" onClick="papercode()"><img id="papercode" src="img/paper codebijgesnedenV1.png"
+//alt="paperCode" height="100px", width="100px"></button>;//
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const myBtn = document.getElementById("myBtn");
+
+myBtn.addEventListener("click", function(e) {
+    console.log("the button was clicked");
+});
